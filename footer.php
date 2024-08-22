3e6f9d3bbe07574f3054a2822d882c99466fb53d
Hamza-alikazmi <63kazmi.edu@gmail.com>
2024-08-22 14:04:48 +0500

    phps
    adding php for mliples pages

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="copyright">
+      <p>&copy; 2024 SKill Spectrum PK. All rights reserved to real owners. Learning Purposes Only</p>
+  </div>
---
+++ header.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en-US" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <meta charset="UTF-8" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Skill Spectrum PK/</title>
+	<link rel="stylesheet" href="style.css">
+
+
+<script type="text/javascript" src="script.js"></script>
+
+  <meta name="description" content="Welcome to Skill Spectrum where you can download free and watch free Courses, " />
+  <link rel="shortcut icon" href="https://yt3.googleusercontent.com/fKTsnDgPjhmqgBr7uu2yuV05s5CnCgKxdF8cFJcGtSwBzTHANRI7wD7QfTaACtiq_QY2A_cl8Q=s160-c-k-c0x00ffffff-no-rj" />
+
+
+<link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+  <meta name='robots' content='max-image-preview:large' />
+
+<!-- This site is optimized with the Yoast SEO Premium plugin v3.1 - https://yoast.com/wordpress/plugins/seo/ -->
+<meta name="description" content="Welcome to Skill Spectrum where you can download free and watch free Courses."/>
+<meta name="robots" content="noodp"/>
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Skill Spectrum PK" />
+<meta property="og:description" content="Welcome to Skill Spectrum where you can download free and wastch free Courses, " />
+<meta property="og:site_name" content="Skkill Spectrum PK" />
+<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","url":"https:\/\/skillspectrumpk.vercel.app\/","name":"Skill Spectrum PK","potentialAction":{"@type":"SearchAction","target":"https:\/\/skillspectrumpk.vercel.app\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
+<meta name="google-site-verification" content="NBkE6T8ajyB4Hq9faAOWw2ruuF3VlSp5TZsaghi7knU" />
+<!-- / Yoast SEO Premium plugin. -->
+
+<link rel='dns-prefetch' href='//stats.wp.com' />
+<link rel='dns-prefetch' href='//www.googletagmanager.com' />
+<link rel="alternate" type="application/rss+xml" title="skillspectrumpk &raquo; Feed" href="##" />
+
+<link rel='stylesheet' id='wp-block-library-css' href='https://oceansofgamess.com/wp-includes/css/dist/block-library/style.min.css?ver=6.5.5' type='text/css' media='all' />
+<style id='wp-block-library-inline-css' type='text/css'>
+.has-text-align-justify{text-align:justify;}
+</style>
+<link rel='stylesheet' id='mediaelement-css' href='https://oceansofgamess.com/wp-includes/js/mediaelement/mediaelementplayer-legacy.min.css?ver=4.2.17' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-mediaelement-css' href='https://oceansofgamess.com/wp-includes/js/mediaelement/wp-mediaelement.min.css?ver=6.5.5' type='text/css' media='all' />
+<style id='jetpack-sharing-buttons-style-inline-css' type='text/css'>
+.jetpack-sharing-buttons__services-list{display:flex;flex-direction:row;flex-wrap:wrap;gap:0;list-style-type:none;margin:5px;padding:0}.jetpack-sharing-buttons__services-list.has-small-icon-size{font-size:12px}.jetpack-sharing-buttons__services-list.has-normal-icon-size{font-size:16px}.jetpack-sharing-buttons__services-list.has-large-icon-size{font-size:24px}.jetpack-sharing-buttons__services-list.has-huge-icon-size{font-size:36px}@media print{.jetpack-sharing-buttons__services-list{display:none!important}}.editor-styles-wrapper .wp-block-jetpack-sharing-buttons{gap:0;padding-inline-start:0}ul.jetpack-sharing-buttons__services-list.has-background{padding:1.25em 2.375em}</style>
+
+
+<link rel='stylesheet' id='crp-style-rounded-thumbs-css' href='https://oceansofgamess.com/wp-content/plugins/contextual-related-posts/css/rounded-thumbs.min.css?ver=3.5.2' type='text/css' media='all' />
+
+
+
+<link rel='stylesheet' id='mystique-core-css' href='https://oceansofgamess.com/wp-content/themes/mystique/css/core.css?ver=3.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='mystique-style-css' href='https://oceansofgamess.com/wp-content/themes/mystique/css/style-green.css?ver=3.3.2' type='text/css' media='all' />
+<script type="text/javascript" src="https://oceansofgamess.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://oceansofgamess.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="jquery-js-after">
+/* <![CDATA[ */
+jQuery(document).ready(function() {
+	jQuery(".d4c6906c9d721da8ea7c6972ff67f5e0").click(function() {
+		jQuery.post(
+ 			"https://oceansofgamess.com/wp-admin/admin-ajax.php", {
+ 				"action": "quick_adsense_onpost_ad_click",
+ 				"quick_adsense_onpost_ad_index": jQuery(this).attr("data-index"),
+ 				"quick_adsense_nonce": "dd857c315a",
+ 			}, function(response) { }
+ 		);
+ 	});
+ });
+ /* ]]> */
+</script>
+<script type="text/javascript" src="https://oceansofgamess.com/wp-content/plugins/ads-disabler-disqus-main/assets/js/frontend.js?ver=1.0.0" id="Disable_Disqus_Ads-frontend-js"></script>
+<script type="text/javascript" src="https://oceansofgamess.com/wp-content/plugins/remove-disqus-ads/assets/js/frontend.js?ver=1.0.0" id="remove_disqus_ads-frontend-js"></script>
+<link rel="https://api.w.org/" href="https://oceansofgamess.com/wp-json/" /><link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://oceansofgamess.com/xmlrpc.php?rsd" />
+<meta name="generator" content="WordPress 6.5.5, ATOM 2.1.2" />
+<meta name="generator" content="Site Kit by Google 1.129.1" />
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111588925-1"></script>
+
+
+<meta name="monetag" content="0bf671dc0cd5fec7ea6b7dc2bc4ddcf6">
+
+
+<script data-cfasync="false" src="//d3ngt858zasqwf.cloudfront.net/?ztgnd=917250"></script>
+<script data-cfasync="false" src="//dt3y1f1i1disy.cloudfront.net/?ifytd=1058645"></script>
+	<style>img#wpstats{display:none}</style>
+		<link rel="icon" href="https://skillspectrumpk.vercel.app/logo.jpg" sizes="32x32" />
+<link rel="icon" href="https://skillspectrumpk.vercel.app/logo.jpg" sizes="192x192" />
+<link rel="apple-touch-icon" href="https://oceansofgamess.com/wp-content/uploads/2017/07/favicon.png" />
+<meta name="msapplication-TileImage" content="https://yt3.googleusercontent.com/fKTsnDgPjhmqgBr7uu2yuV05s5CnCgKxdF8cFJcGtSwBzTHANRI7wD7QfTaACtiq_QY2A_cl8Q=s160-c-k-c0x00ffffff-no-rj" />
+
+</head>
+
+<body>
+    <header>
+        <div class="logo-container" style="display: block; ">
+            <a href="index.php">
+                <img src="https://skillspectrumpk.vercel.app/logo.jpg" alt="Your Company Logo" class="logo" width="100%" height="100%">
+            </a>
+        </div>
+       <div> 
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav></div>
+    </header>
